90faa88f304c264d2cd38bb49852bcf4eee236d3
alemaidad <maisdel2323@gmail.com>
2026-03-16 07:35:32 -0500

    Simulador de Gestor de archivos no esta integrado al SO

--- drivers/video.h
+++ drivers/video.h
@@ -6,7 +6,7 @@
 #define VIDEO_ADDRESS 0XB8000 // Direccion de memoria de video para texto en modo texto
 #define MAX_ROWS 25 // Numero maximo de filas en la pantalla
 #define MAX_COLS 80 // Numero maximo de columnas en la pantalla
-#define WHITE_ON_BLACK 0X0f // Atributo de color para texto blanco
+#define WHITE_ON_BLACK 0x0a // Atributo de color para texto verde
 
 void kprint_char(char character, int col, int row, char attribute_byte);
 void kprint(char* message);
